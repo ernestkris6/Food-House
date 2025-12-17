0df98d2b77b5fa7b96a941bac5d5c891dffea350
ERNEST CHRIS <ernestchris6@gmail.com>
2025-12-18 00:54:49 +0100

    hero section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from '../UI/HeroSection'
 import Details from '../UI/Details'
+import Products from '../UI/Products'
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
       <Details />
+      <Products />
     </div>
   )
 }
--- src/UI/Products.jsx
+++ src/UI/Products.jsx
@@ -1,31 +1,75 @@
-// {imageDetails.map((item, idx)=> (
-//               <Gallery key={idx} text={item.text}/>
-//             ))}
+import hero from '../assets/hero.jpg'
 
-// function Gallery ({img, alt, text}) {
-//   return(
-//       <div>
-//         <b>{text}</b>
-//         <img src={img} alt={alt} />
-//       </div>
-//   )
-//  } 
+const imageDetails = [
+  {
+    id: 0,
+    text: "Food",
+    img: hero,
+  },
+    {
+    id: 2,
+    text: "Food",
+    img: hero,
+  },
+    {
+    id: 3,
+    text: "Food",
+    img: hero,
+  },
+  {
+    id: 3,
+    text: "Food",
+    img: hero,
+  },
+  {
+    id: 3,
+    text: "Food",
+    img: hero,
+  },
+  {
+    id: 3,
+    text: "Food",
+    img: hero,
+  },
+  {
+    id: 3,
+    text: "Food",
+    img: hero,
+  },
+  {
+    id: 3,
+    text: "Food",
+    img: hero,
+  },
+  {
+    id: 3,
+    text: "Food",
+    img: hero,
+  },
+  
+
+]
 
 
-// const imageDetails = [
-//   {
-//     id: 0,
-//     text: "Food",
-//     img: "",
-//   },
-//     {
-//     id: 2,
-//     text: "Food",
-//     img: "",
-//   },
-//     {
-//     id: 3,
-//     text: "Food",
-//     img: "",
-//   }
-// ]
+
+function Gallery ({src, alt, text}) {
+  return(
+      <div className='flex-shrink-0 snap-start w-48 sm:w-56 md:w-60 lg:w-64 cursor-pointer rounded-md overflow-hidden group'>
+        <img className='rounded' src={src} alt={alt} />
+        <b>{text}</b>
+      </div>
+  )
+ } 
+
+export default function Products() {
+  return (
+    <div className='bg-[#E7f5f5] py-12'>
+        <div className='flex overflow-x-auto gap-4 snap-x snap-mandatory scrollbar-hide'>
+            {imageDetails.map((item, idx)=> (
+              <Gallery src={item.img} key={idx} text={item.text}/>
+            ))}
+        </div>
+        <button className='bg-[#f3274c] text-white rounded px-6 py-2 mt-4 cursor-pointer'>Start Ordering</button>
+    </div>
+  )
+}
